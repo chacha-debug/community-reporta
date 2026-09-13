f08f560a175939f4ada6fd87286ca2683bf88635
Chantele Mucuio <chantelemucuio@example.com>
2026-09-13 17:19:32 +0200

    Remove superuser credentials from build script

--- build_files.sh
+++ build_files.sh
@@ -4,14 +4,3 @@ set -e
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-
-# Create superuser if it doesn't already exist
-python manage.py shell -c "
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'cmucuio@gmail.com', 'Kimberley#2026!Reporta!')
-    print('Superuser created')
-else:
-    print('Superuser already exists')
-"
